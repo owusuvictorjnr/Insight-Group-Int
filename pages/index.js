100de5faccc968327640210f0854f91c710ed5ea
owusuvictorjnr <owusuvictor80@gmail.com>
2023-02-05 08:57:45 +0000

    Feature: Home

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Home from '@/components/Home';
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+};
+
+export default HomePage;
