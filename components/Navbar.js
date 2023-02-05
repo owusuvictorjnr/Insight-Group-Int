5b93fb391e29ac7b306354b8ada825bb4faec991
owusuvictorjnr <owusuvictor80@gmail.com>
2023-02-05 08:56:17 +0000

    Feature: Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div className="bg-[#f5ebe0] h-36 border-b flex items-center top-0 bottom-0 ">
+      <div className="flex justify-evenly  w-full">
+        <div className="flex gap-5">
+          <Link href={'/'}>
+            <Image
+              src="/logo/logo1.jpg"
+              alt="logo1"
+              width="70"
+              height="70"
+              className="rounded-full "
+            />
+          </Link>
+         
+          <p className="capitalize flex items-center text-3xl text-[#1d3557]">
+            <Link href={'/'}>insight group int.</Link>
+          </p>
+        </div>
+
+        <div className="flex items-center gap-10 lg:gap-20 capitalize text-2xl">
+          <Link href={'/'} className="">
+            <ul className="cursor-pointer py-2 px-2 hover:border-[#1d3557] hover:bg-[#d6ccc2] rounded-xl hover:text-[#bc6c25] text-[#ffb703] hover:underline duration-500  ">
+              <li>home</li>
+            </ul>
+          </Link>
+
+          <Link href={'/about'}>
+            <ul className="cursor-pointer py-2 px-2 hover:border-[#1d3557] hover:bg-[#d6ccc2] rounded-xl hover:text-[#bc6c25] text-[#ffb703] hover:underline duration-500  ">
+              <li>about</li>
+            </ul>
+          </Link>
+
+          <Link href={'/contact'}>
+            <ul className="cursor-pointer py-2 px-2 hover:border-[#1d3557] hover:bg-[#d6ccc2] rounded-xl hover:text-[#bc6c25] text-[#ffb703] hover:underline duration-500  ">
+              <li>contact</li>
+            </ul>
+          </Link>
+
+          <Link href={'/sponsors'}>
+            <ul className="cursor-pointer py-2 px-2 hover:border-[#1d3557] hover:bg-[#d6ccc2] rounded-xl hover:text-[#bc6c25] text-[#ffb703] hover:underline duration-500  ">
+              <li>sponsors</li>
+            </ul>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
