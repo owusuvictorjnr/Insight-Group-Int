ced217681264f109bcd02eb61f00ac41fe4a15ac
owusuvictorjnr <owusuvictor80@gmail.com>
2023-02-06 17:59:39 +0000

    Feature: Events updates

--- components/Home.js
+++ components/Home.js
@@ -4,14 +4,68 @@ import { RiHandHeartLine } from 'react-icons/ri';
 import { IoRibbonOutline } from 'react-icons/io5';
 import { GrAchievement } from 'react-icons/gr';
 import { BsPiggyBank } from 'react-icons/bs';
+import Image from 'next/image';
+import DateTime from './Datetime';
 
 const Home = () => {
   return (
-    <div className="h-screen w-screen bg-[#f1faee]">
-      <div className="min-h-[40rem] bg-black w-screen ">
-        {/*<LandingPage />*/}
+    <div className=" w-screen bg-[#ffffff] flex flex-col">
+      <div className="min-h-[40rem]   mt-[5rem] flex justify-center ">
+        <LandingPage />
       </div>
 
+      {/*main card*/}
+      <div className="mt-10 min-h-[40rem]">
+        <div className="">
+          <div className="flex justify-evenly  ">
+            {/*welcome card */}
+            <div className="mt-[10rem]">
+              <div className="px-10 container mx-auto ">
+                <h1 className="capitalize text-3xl  text-[#3a0ca3] underline">
+                  welcome to insight group international
+                </h1>
+              </div>
+              <p className="mt-10 container mx-auto px-5  lg:mx-20 lg:py-5 py-5 tracking-widest text-[#34a0a4]">
+                Power modern applications with enriched querying capabilities,
+                <br />
+                new operators, added encryption features and more.
+              </p>
+
+              {/*3 cards under welcome*/}
+              <div className="grid lg:grid-cols-3 md:grid-cols-2 m-5 container mx-auto">
+                <div className="h-40 w-40 m-5 mb-3 border-2">
+                  <p>total donation</p>
+                </div>
+
+                <div className="h-40 w-40 m-5 mb-3 border-2">
+                  <p>total donation</p>
+                </div>
+
+                <div className="h-40 w-40  m-5 mb-3 border-2">
+                  <p>total donation</p>
+                </div>
+              </div>
+            </div>
+
+            {/*right image card*/}
+            <div className="mt-[10rem] h-full bg-red-500  ">
+              <div className="container mx-auto text-center">
+                <p className="text-end">
+                  <Image
+                    src={'/img/pic2.jpg'}
+                    alt="image"
+                    height={500}
+                    width={500}
+                    className="w-full h-full "
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* how could you help*/}
       <div className=" min-h-[20rem] ">
         <div className="flex justify-center pt-20">
           <h1 className="capitalize text-3xl text-[#3a0ca3]">
@@ -20,7 +74,7 @@ const Home = () => {
         </div>
         <div className=" px-10 pt-[5rem] flex justify-center items-center">
           <div className="grid  md:grid-cols-2 gap-8">
-            <div className="lg:w-[30rem] w-[25rem] h-[15rem] bg-white  rounded-md mb-5 container mx-auto ">
+            <div className="lg:w-[30rem] w-[25rem] h-[15rem] bg-[#f1faee]  rounded-md mb-5 container mx-auto ">
               <div className="flex justify-center capitalize">
                 <h2 className="flex justify-center pt-10 gap-3 text-2xl text-[#34a0a4]">
                   <RiHandHeartLine size={30} color={'#76c893'} /> give donation
@@ -28,7 +82,7 @@ const Home = () => {
               </div>
             </div>
 
-            <div className="lg:w-[30rem] w-[25rem]  h-[15rem] bg-white  rounded-md mb-5 container mx-auto">
+            <div className="lg:w-[30rem] w-[25rem]  h-[15rem] bg-[#f1faee]  rounded-md mb-5 container mx-auto">
               <div className="flex justify-center capitalize">
                 <h2 className="flex justify-center pt-10 gap-3 text-2xl text-[#34a0a4]">
                   <IoRibbonOutline size={30} color={'#76c893'} />
@@ -41,7 +95,7 @@ const Home = () => {
 
         <div className="px-10  flex justify-center">
           <div className="grid  md:grid-cols-2 gap-8">
-            <div className="w-[25rem] lg:w-[30rem] h-[15rem] bg-white  rounded-md mb-5 container mx-auto ">
+            <div className="w-[25rem] lg:w-[30rem] h-[15rem] bg-[#f1faee]  rounded-md mb-5 container mx-auto ">
               <div className="flex justify-center capitalize">
                 <h2 className="flex justify-center pt-10 gap-3 text-2xl text-[#34a0a4]">
                   <GrAchievement size={30} color={'#76c893'} />
@@ -50,7 +104,7 @@ const Home = () => {
               </div>
             </div>
 
-            <div className="w-[25rem] lg:w-[30rem] h-[15rem] bg-white  rounded-md mb-5 container mx-auto">
+            <div className="w-[25rem] lg:w-[30rem] h-[15rem] bg-[#f1faee]  rounded-md mb-5 container mx-auto">
               <div className="flex justify-center capitalize">
                 <h2 className="flex justify-center pt-10 gap-3 text-2xl text-[#34a0a4]">
                   <BsPiggyBank size={30} color={'#76c893'} /> quick fundraise
@@ -58,6 +112,68 @@ const Home = () => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className=" min-h-[30rem] mb-10">
+        <div className="flex justify-center px-10 pt-10 ">
+          <Image
+            src={'/img/pic6.jpg'}
+            alt="bg pic"
+            height={'600'}
+            width={600}
+            className="bg-white w-full h-full rounded-md lg:h-[40rem] "
+          />
+          {/*  <p className="">
+              forget what you can get and see what you can give
+              </p>*/}
+        </div>
+        <div className="flex justify-center py-3 ">
+          <button className="">
+            <p className="bg-[#06ec57] py-3 px-3 capitalize text-[#ffffff] rounded-full hover:bg-[#ffffff] hover:text-[#06ec57] border-[#06ec57] border-2 duration-500">
+              become a volunteer
+            </p>
+          </button>
+        </div>
+      </div>
+
+      {/*event*/}
+      <div className="min-h-[35rem] bg-slate-500 mb-5">
+        <div className="flex justify-center pt-[8rem] capitalize text-3xl text-[#3a0ca3]">
+          <h1>upcoming events</h1>
+        </div>
+        <div className="flex justify-center pt-8">
+          <p className="text-[#34a0a4] text-center text-sm">
+            Power modern applications with enriched querying capabilities,
+            <br />
+            new operators, added encryption features and more.
+          </p>
+        </div>
+
+        <div className="mt-20 flex justify-between container mx-auto">
+          <div className="flex gap-5">
+            <Image
+              src={'/img/pic9.jpg'}
+              alt={'event1'}
+              height={500}
+              width={500}
+              className="rounded-xl mb-10"
+            />
+            <div>
+              <DateTime />
+              <h1 className="capitalize lg:text-2xl text-[#ffffff] pt-10 mx-5 flex flex-col">
+                today is a volunteers day
+              </h1>
+
+              <p className="text-[#34a0a4] text-center text-sm">
+                Power modern applications with enriched querying capabilities,
+                <br />
+                new operators, added encryption features and more.
+              </p>
+            </div>
+          </div>
+
+          <div>come</div>
         </div>
       </div>
     </div>
