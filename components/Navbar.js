1aa14e10b986c29fe0a1d3e807d720c0fdf2fcbd
owusuvictorjnr <owusuvictor80@gmail.com>
2023-02-19 05:38:41 +0000

    Fix: change navbar to fixed and background color to light gray

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,15 +4,15 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <div className=" h-36  items-center flex  top-0 z-20 left-0 right-0 ">
+    <div className="bg-veryLightGray h-36 items-center flex top-0 z-20 left-0 right-0 fixed border shadow-lg  ">
       <div className="flex justify-evenly  w-full">
         <div className="flex gap-5">
           <Link href={'/'}>
             <Image
               src="/logo/logo1.jpg"
               alt="logo1"
-              width={"100"}
-              height={"100"}
+              width={'100'}
+              height={'100'}
               className="rounded-full "
             />
           </Link>
@@ -37,13 +37,13 @@ const Navbar = () => {
             </Link>
 
             <Link href={'/contact'}>
-              <ul className="cursor-pointer py-2 px-2 hover:border-[#1d3557] hover:bg-[#d6ccc2] rounded-xl hover:text-[#bc6c25] text-[#ffb703] hover:underline duration-500  ">
+              <ul className="cursor-pointer py-2 px-2  hover:bg-brightRedLight rounded-xl hover:text-brightRedSupLight text-white bg-brightRed hover:underline duration-500  md:block  ">
                 <li>contact</li>
               </ul>
             </Link>
 
             <Link href={'/sponsors'}>
-              <ul className="cursor-pointer py-2 px-2 hover:border-[#1d3557] hover:bg-[#d6ccc2] rounded-xl hover:text-[#bc6c25] text-[#ffb703] hover:underline duration-500  ">
+              <ul className="cursor-pointer py-2 px-2  hover:bg-brightRedLight rounded-xl hover:text-brightRedSupLight text-white bg-brightRed hover:underline duration-500  md:block ">
                 <li>sponsors</li>
               </ul>
             </Link>
