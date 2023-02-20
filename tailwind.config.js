3d3fec71433793cd334f36d2f5ef90765aec13e0
owusuvictorjnr <owusuvictor80@gmail.com>
2023-02-20 16:17:38 +0000

    Fix: Remove custom screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,6 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    screens: {
-      sm: '640px',
-
-      md: '768px',
-
-      lg: '1024px',
-
-      xl: '1280px',
-
-      '2xl': '1536px',
-    },
-
     extend: {
       colors: {
         brightRed: 'hsl(12, 88%, 59%)',
