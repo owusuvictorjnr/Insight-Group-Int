f1ec995a717e1b17f2bdfa8db2ec519a377f89c7
owusuvictorjnr <owusuvictor80@gmail.com>
2023-02-22 23:53:38 +0000

    Feature: impact

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import Navbar from '@/components/Navbar';
 import React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
+import { MdOutlineStarOutline } from 'react-icons/md';
 
 const About = () => {
   return (
@@ -32,14 +33,14 @@ const About = () => {
               alt="/"
               height="300"
               width="400"
-              className="rounded-t-3xl mx-10 lg:pb-10 w-[30rem] h-[15rem] lg:w-full lg:h-full"
+              className="rounded-t-3xl mx-10 w-[24rem] h-[13rem] md:w-full md:h-[20rem] md:px-3 lg:w-[45rem] lg:h-[30rem] lg:px-1"
             />
           </div>
           <div className="flex justify-center rounded-t-xl container mx-auto md:pt-10 lg:pt-10">
             <div className="flex flex-col container mx-auto justify-center items-center px-10 lg:items-center md:items-center text-sm lg:text-3xl md:text-2xl">
-              <p className="capitalize text-brightRedLight font-bold">
+              <h1 className="capitalize text-brightRedLight font-bold text-sm md:text-2xl lg:text-3xl ">
                 our mission
-              </p>
+              </h1>
 
               <div className="text-sm text-darkBlue lg:text-3xl md:text-xl">
                 To Share Love with everyone by providing nourishing delicious
@@ -66,8 +67,59 @@ const About = () => {
               alt="/"
               width={'300'}
               height={'300'}
-              className="rounded flex px-10 w-full h-[13rem] md:w-full md:h-[25rem] md:px-5 lg:w-[40rem] lg:h-[40rem] "
+              className="rounded-r-full md:rounded-r-full flex mx-10 w-full h-[13rem] md:w-full md:h-[20rem] md:px-3 lg:w-[45rem] lg:h-[30rem] lg:px-1"
             />
+          </div>
+        </div>
+
+        {/*Our impact*/}
+        <div className="pt-10 mb-10 grid grid-cols-1 md:grid-cols-2">
+          <div className="container m-auto flex justify-center ">
+            <Image
+              src={'/img/pic6.jpg'}
+              alt={'/'}
+              width="300"
+              height="300"
+              className="rounded-l-[4rem] md:rounded-l-[4rem] w-[24rem] h-[13rem] md:w-full md:h-[20rem] md:px-3 llg:w-[45rem] lg:h-[30rem] lg:px-5"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center container mx-auto pt-3">
+            <h1 className="text-sm text-brightRedLight capitalize font-bold md:text-2xl lg:text-3xl">
+              our impact
+            </h1>
+
+            <div>
+              <div className="flex justify-center container mx-auto px-10 pt-1">
+                <MdOutlineStarOutline size={'25'} color={'red'} />
+                <p className="container mx-auto flex justify-center px-1 text-darkBlue md:text-xl lg:text-3xl">
+                  As a heart-centered brand that cares about its global impact
+                </p>
+              </div>
+              <div className="flex justify-center container mx-auto px-10 pt-5">
+                <MdOutlineStarOutline size={'25'} color={'red'} />
+                <p className="container mx-auto flex justify-center px-1 text-darkBlue md:text-xl lg:text-3xl">
+                  As a heart-centered brand that cares about its global impact
+                </p>
+              </div>
+              <div className="flex justify-center container mx-auto px-10  pt-5">
+                <MdOutlineStarOutline size={'25'} color={'red'} />
+                <p className="container mx-auto flex justify-center px-1 text-darkBlue md:text-xl lg:text-3xl">
+                  As a heart-centered brand that cares about its global impact
+                </p>
+              </div>
+              <div className="flex justify-center container mx-auto px-10  pt-5">
+                <MdOutlineStarOutline size={'25'} color={'red'} />
+                <p className="container mx-auto flex justify-center px-1 text-darkBlue md:text-xl lg:text-3xl">
+                  As a heart-centered brand that cares about its global impact
+                </p>
+              </div>
+              <div className="flex justify-center container mx-auto px-10  pt-5">
+                <MdOutlineStarOutline size={'25'} color={'red'} />
+                <p className="container mx-auto flex justify-center px-1 text-darkBlue md:text-xl lg:text-3xl">
+                  As a heart-centered brand that cares about its global impact
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
